9c4a1a392fdf70b0504823ee9a30833e8bd7b1e2
julijazelenuhina <41117621+julijazelenuhina@users.noreply.github.com>
2018-07-13 13:22:47 +0300

    Update Shows.java
    Add check-ups

--- citsProjekts/src/main/java/hello/Shows.java
+++ citsProjekts/src/main/java/hello/Shows.java
@@ -3,20 +3,22 @@ package hello;
 import java.sql.Date;
 
 public class Shows {
-	public int show_id;
-	public int cinema_id;
-	public int movie_id;
-	public int room_id;
-	public Date date;
-	public String taken_seats;
+	private int show_id;
+	private int cinema_id;
+	private int movie_id;
+	private int room_id;
+	private Date date;
+	private String taken_seats;
 	
-	public Shows(int sid, int cid, int mid, int rid, Date date, String taken_seats) {
-		this.show_id=sid;
-		this.cinema_id=cid;
-		this.movie_id=mid;
-		this.room_id=rid;
-		this.date=date;
-		this.taken_seats=taken_seats;
+	public Shows() {} //constructor for testing purposes
+
+	public Shows(int show_id, int cinema_id, int movie_id, int room_id, Date date, String taken_seats) {
+		setShow_id(show_id);
+		setCinema_id(cinema_id);
+		setMovie_id(movie_id);
+		setRoom_id(room_id);
+		setDate(date);
+	    setTaken_seats(taken_seats);
 	}
 
 	public int getShow_id() {
@@ -24,7 +26,11 @@ public class Shows {
 	}
 
 	public void setShow_id(int show_id) {
-		this.show_id = show_id;
+		if(show_id <= 0) {
+			throw new IllegalArgumentException();
+		}else {
+			this.show_id = show_id;
+		}
 	}
 
 	public int getCinema_id() {
@@ -32,7 +38,11 @@ public class Shows {
 	}
 
 	public void setCinema_id(int cinema_id) {
-		this.cinema_id = cinema_id;
+		if(cinema_id <= 0) {
+			throw new IllegalArgumentException();
+		}else {
+			this.cinema_id = cinema_id;
+		}
 	}
 
 	public int getMovie_id() {
@@ -40,7 +50,11 @@ public class Shows {
 	}
 
 	public void setMovie_id(int movie_id) {
-		this.movie_id = movie_id;
+		if(movie_id <= 0) {
+			throw new IllegalArgumentException();
+		}else {
+			this.movie_id = movie_id;
+		}
 	}
 
 	public int getRoom_id() {
@@ -48,7 +62,11 @@ public class Shows {
 	}
 
 	public void setRoom_id(int room_id) {
-		this.room_id = room_id;
+		if(room_id <= 0) {
+			throw new IllegalArgumentException();
+		}else {
+			this.room_id = room_id;
+		}
 	}
 
 	public Date getDate() {
@@ -64,6 +82,11 @@ public class Shows {
 	}
 
 	public void setTaken_seats(String taken_seats) {
-		this.taken_seats = taken_seats;
+		if(taken_seats == "") {
+			throw new IllegalArgumentException();
+		}else {
+			this.room_id = room_id;
+		}
 	}
 }
+
