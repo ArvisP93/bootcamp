ee21c324191ffa2b2deb193c4b8a7e08b4d5f939
julijazelenuhina <41117621+julijazelenuhina@users.noreply.github.com>
2018-07-13 13:48:08 +0300

    Add TestShows.java
    JUnit tests for Shows.java

---
+++ citsProjekts/src/test/java/hello/TestShows.java
@@ -0,0 +1,100 @@
+package com.example.demo;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Date;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.modelclasses.Shows;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestShows {
+	
+	@Test
+	public void testShowId() {
+		Shows obj = new Shows();
+		try {
+			obj.setShow_id(0);
+			fail("Exception was expected for 0 show_id input");
+		}
+		catch(IllegalArgumentException e) {
+		}
+		try {
+			obj.setShow_id(-11);
+			fail("Exception was expected for negative show_id input");
+		}
+		catch(IllegalArgumentException e) {
+		}
+	}
+	
+	@Test
+	public void testCinemaId() {
+		Shows obj = new Shows();
+		try {
+			obj.setCinema_id(0);
+			fail("Exception was expected for 0 cinema_id input");
+		}
+		catch(IllegalArgumentException e) {
+		}
+		try {
+			obj.setCinema_id(-11);
+			fail("Exception was expected for negative cinema_id input");
+		}
+		catch(IllegalArgumentException e) {
+		}
+	}
+	
+	@Test
+	public void testMovieId() {
+		Shows obj = new Shows();
+		try {
+			obj.setMovie_id(0);
+			fail("Exception was expected for 0 id input");
+		}
+		catch(IllegalArgumentException e) {
+		}
+		try {
+			obj.setMovie_id(-10);
+			fail("Exception was expected for negative id input");
+		}
+		catch(IllegalArgumentException e) {
+		}
+	
+	}
+	
+	@Test
+	public void testRoomId() {
+		Shows obj = new Shows();
+		try {
+			obj.setRoom_id(0);
+			fail("Exception was expected for 0 room_id input");
+		}
+		catch(IllegalArgumentException e) {
+		}
+		try {
+			obj.setRoom_id(-11);
+			fail("Exception was expected for negative room_id input");
+		}
+		catch(IllegalArgumentException e) {
+		}
+	
+	}
+	
+	@Test
+	public void testTakenSeats() {
+		Shows obj = new Shows();
+		try {
+			obj.setTaken_seats("");
+			fail("Exception was expected for empty taken_seats input");
+		}
+		catch(IllegalArgumentException e) {
+		}
+	}
+
+
+}
