172436742d8d7188d5c46224474c8c684bd62817
julijazelenuhina <41117621+julijazelenuhina@users.noreply.github.com>
2018-07-13 11:35:29 +0300

    Update Rooms.java
    Add check-ups

--- citsProjekts/src/main/java/hello/Rooms.java
+++ citsProjekts/src/main/java/hello/Rooms.java
@@ -1,47 +1,66 @@
 package hello;
 
 public class Rooms {
-	public int room_id;
-	public int cinema_id;
-	public String name;
-	public int seats;
-	
-	public Rooms(int rid, int cid, String name, int seats) {
-		this.room_id=rid;
-		this.cinema_id=cid;
-		this.name=name;
-		this.seats=seats;
+		private int room_id;
+		private int cinema_id;
+		private String name;
+		private int seats;
+		
+		public Rooms() {} //constructor for testing purposes
+		
+		public Rooms(int rid, int cid, String name, int seats) {
+			setRoom_id(cid);
+			setCinema_id(cid);
+			setName(name);
+			setSeats(seats);
+		}
+
+		public int getRoom_id() {
+			return room_id;
+		}
+
+		public void setRoom_id(int room_id) {
+			if(room_id <= 0) {
+				throw new IllegalArgumentException();
+			}else {
+				this.room_id = room_id;
+			}
+		}
+
+		public int getCinema_id() {
+			return cinema_id;
+		}
+
+		public void setCinema_id(int cinema_id) {
+			if(cinema_id <= 0) {
+				throw new IllegalArgumentException();
+			}else {
+				this.cinema_id = cinema_id;
+			}
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			if (name == "") {
+				throw new IllegalArgumentException();
+			} else {
+				this.name = name;;
+			}
+		}
+
+		public int getSeats() {
+			return seats;
+		}
+
+		public void setSeats(int seats) {
+			if(seats <= 0) {
+				throw new IllegalArgumentException();
+			}else {
+				this.seats = seats;
+			}
+		}
 	}
 
-	public int getRoom_id() {
-		return room_id;
-	}
-
-	public void setRoom_id(int room_id) {
-		this.room_id = room_id;
-	}
-
-	public int getCinema_id() {
-		return cinema_id;
-	}
-
-	public void setCinema_id(int cinema_id) {
-		this.cinema_id = cinema_id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public int getSeats() {
-		return seats;
-	}
-
-	public void setSeats(int seats) {
-		this.seats = seats;
-	}
-}
