60b752609ce08d01649558fbadd29f3da297cde3
julijazelenuhina <41117621+julijazelenuhina@users.noreply.github.com>
2018-07-13 14:33:22 +0300

    Update TestUsers.java

--- citsProjekts/src/test/java/hello/TestUsers.java
+++ citsProjekts/src/test/java/hello/TestUsers.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package hello;
 
 import static org.junit.Assert.fail;
 
@@ -7,8 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.modelclasses.Shows;
-import com.modelclasses.Users;
+import hello.Users;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
