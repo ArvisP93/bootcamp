132e0e563a2fc08d25acccef7862efe6ad9bbdb6
julijazelenuhina <41117621+julijazelenuhina@users.noreply.github.com>
2018-07-19 15:32:25 +0300

    Update TestCinemas.java

--- citsProjekts3/src/test/java/hello/TestCinemas.java
+++ citsProjekts3/src/test/java/hello/TestCinemas.java
@@ -10,8 +10,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import com.example.cbs.Cinemas;
 
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class TestCinemas {
 	
 	@Test
