c7d955cd2d1911e70397393fdb5d8119fcf81fc2
julijazelenuhina <41117621+julijazelenuhina@users.noreply.github.com>
2018-07-19 15:28:23 +0300

    Add TestSeat.java
    Add JUnit tests for class Seat.java

---
+++ citsProjekts3/src/test/java/hello/TestSeat.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+import org.junit.Test;
+
+import hello.Cinemas;
+import hello.Seat;
+
+public class TestSeat {
+	
+	@Test
+	public void testConstructor() {
+		Seat obj = new Seat(-11, true);
+		assertEquals(obj.getId(), 0);
+		assertEquals(obj.isTaken(), true);
+		obj.setId(25);
+		obj.setTaken(false);
+		assertEquals(obj.getId(), 25);
+		assertEquals(obj.isTaken(), false);
+	}
+
+	@Test
+	public void testId() {
+		Seat obj = new Seat();
+		obj.setId(-11);
+		assertEquals(obj.getId(), 0);
+		obj.setId(0);
+		assertEquals(obj.getId(), 0);
+		obj.setId(25);
+		assertEquals(obj.getId(), 25);
+	}
+	
+}
