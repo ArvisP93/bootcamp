de6b7d8a1e9c6c345c3fe69dfe7339417ba1cb49
julijazelenuhina <41117621+julijazelenuhina@users.noreply.github.com>
2018-07-19 15:31:03 +0300

    Update TestRooms.java

--- citsProjekts3/src/test/java/hello/TestRooms.java
+++ citsProjekts3/src/test/java/hello/TestRooms.java
@@ -7,12 +7,10 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.example.cbs.Cinemas;
-import com.example.cbs.Movies;
 import com.example.cbs.Rooms;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 
 public class TestRooms {
 	
